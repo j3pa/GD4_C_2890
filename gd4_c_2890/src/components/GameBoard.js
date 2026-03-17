170404a3b07f37b2a263b8f72c9999b0349535f9
j3pa <yeezepah@gmail.com>
2026-03-17 12:09:27 +0700

    update
    update gameboard.js

--- gd4_c_2890/src/components/GameBoard.js
+++ gd4_c_2890/src/components/GameBoard.js
@@ -1,17 +1,14 @@
 import React from "react";
 import Card from "./Card";
 
-// Jumlah kolom grid sesuai difficulty
-// easy: 4 kartu = 4 kolom (1 baris)... tapi lebih bagus 4 kolom 2 baris
-// medium: 12 kartu = 4 kolom 3 baris
-// hard: 16 kartu = 4 kolom 4 baris
+
 const COLS_MAP = {
   easy:   4,
   medium: 4,
   hard:   4,
 };
 
-// Tailwind grid class sesuai jumlah kolom
+
 const GRID_COLS_CLASS = {
   4: 'grid-cols-4',
 };
